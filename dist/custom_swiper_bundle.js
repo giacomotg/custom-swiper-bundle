2a0bbc61e1d4a256b06d92496d92dfadab738492
Giacomo Ghislandi <giacomotg@gmail.com>
2023-09-30 21:28:20 -0300

    Updated custom_swiper_bundle.js in dist directory

--- dist/custom_swiper_bundle.js
+++ dist/custom_swiper_bundle.js
@@ -1,14 +1,16 @@
-// core version + navigation, pagination modules:
-import Swiper, { Navigation, Pagination } from 'swiper';
-Swiper.use([Navigation, Pagination]);
+// Import necessary components and modules
+import Swiper, { Default } from 'swiper';
 
-// import Swiper and modules styles
-import 'swiper/swiper-bundle.css';
+// Register the modules
+Swiper.use([Navigation, Pagination, Thumbs]);
 
-// init Swiper:
-var heroThumbs = new Swiper(".hero-thumbs", {
-  // configure Swiper to use modules
-    direction: "vertical",
+import 'swiper/swiper-bundle.css'
+
+// 3. Swiper configurations
+
+// Initialize heroThumbs Swiper first
+const heroThumbsSwiper = initSwiper(".hero-thumbs", {
+  direction: "vertical",
   slidesPerView: "auto",
   spaceBetween: 5,
   allowSlideNext: false,
@@ -16,157 +18,158 @@ var heroThumbs = new Swiper(".hero-thumbs", {
   threshold: 12,
   freeMode: true,
   watchSlidesProgress: true,
+  navigation: {
+    nextEl: ".hero-thumbs-next",
+    prevEl: ".hero-thumbs-prev",
+  },
+  pagination: {
+    el: ".hero-thumbs-pagination",
+  },
 });
-var heroCarousel = new Swiper(".hero-carousel", {
-     // configure Swiper to use modules
-  
+
+// Initialize heroCarousel Swiper with reference to heroThumbsSwiper
+const heroCarouselSwiper = initSwiper(".hero-carousel", {
   slidesPerView: "auto",
   spaceBetween: 6,
   pagination: {
     el: ".hero-carousel-pagination",
   },
+  navigation: {
+    nextEl: ".hero-carousel-next",
+    prevEl: ".hero-carousel-prev",
+  },
   breakpoints: {
-  	990: {
-    	slidesPerView: 1,
-  		spaceBetween: 0,
+    990: {
+      slidesPerView: 1,
+      spaceBetween: 0,
     },
   },
   thumbs: {
-    swiper: heroThumbs,
+    swiper: heroThumbsSwiper,
   },
 });
 
-var testiSwiper = new Swiper(".all-testimonials", {
-     // configure Swiper to use modules
-     
+// testiSwiper
+const testiSwiper = initSwiper(".all-testimonials", {
   slidesPerView: "auto",
   spaceBetween: 20,
   pagination: {
     el: ".testimonials-pagination",
   },
   navigation: {
-  	prevEl: '.testimonials-sec .arrow-prev',
-    nextEl: '.testimonials-sec .arrow-next'
+    prevEl: ".testimonials-sec .arrow-prev",
+    nextEl: ".testimonials-sec .arrow-next",
   },
 });
 
-var genSwiper = new Swiper(".all-general-reviews", {
-     // configure Swiper to use modules
-    
-  slidesPerView: 'auto',
+// genSwiper
+const genSwiper = initSwiper(".all-general-reviews", {
+  slidesPerView: "auto",
   spaceBetween: 12,
   centeredSlides: true,
   loop: true,
   loopedSlides: 2,
   navigation: {
-    nextEl: '.general-reviews-next',
-    prevEl: '.general-reviews-prev',
+    nextEl: ".general-reviews-next",
+    prevEl: ".general-reviews-prev",
   },
   pagination: {
-  	el: '.general-reviews-pagination'
+    el: ".general-reviews-pagination",
   },
 });
 
-var uspSwiper = new Swiper(".all-manual-usps", {
-    // configure Swiper to use modules
- 
-    slidesPerView: "auto",
-    spaceBetween: 12,
-    centerInsufficientSlides: true,
-  });
+// uspSwiper
+const uspSwiper = initSwiper(".all-manual-usps", {
+  slidesPerView: "auto",
+  spaceBetween: 12,
+  centerInsufficientSlides: true,
+});
 
-  var howStepSwiper = new Swiper(".all-how-to-steps", {
-     // configure Swiper to use modules
-    
-    slidesPerView: "auto",
-    spaceBetween: 16,
-    pagination: {
-      el: ".how-to-pagination",
-    },
-  });
+// howStepSwiper
+const howStepSwiper = initSwiper(".all-how-to-steps", {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  pagination: {
+    el: ".how-to-pagination",
+  },
+});
 
-  var prodSwiper = new Swiper(".all-more-products", {
-     // configure Swiper to use modules
-  
-    slidesPerView: "auto",
-    spaceBetween: 20,
-    pagination: {
-      el: ".more-products-pagination",
-    },
-    navigation: {
-        prevEl: '.more-products-sec .arrow-prev',
-      nextEl: '.more-products-sec .arrow-next'
-    },
-  });
+// prodSwiper
+const prodSwiper = initSwiper(".all-more-products", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  pagination: {
+    el: ".more-products-pagination",
+  },
+  navigation: {
+    prevEl: ".more-products-sec .arrow-prev",
+    nextEl: ".more-products-sec .arrow-next",
+  },
+});
 
-  var planSwiper = new Swiper(".all-plan-cards", {
-     // configure Swiper to use modules
-    
-    slidesPerView: "auto",
-    spaceBetween: 16,
-    //centerInsufficientSlides: true,
-    breakpoints: {
-      768: {
-        spaceBetween: 30,
-      },
+// planSwiper
+const planSwiper = initSwiper(".all-plan-cards", {
+  slidesPerView: "auto",
+  spaceBetween: 16,
+  breakpoints: {
+    768: {
+      spaceBetween: 30,
     },
-    pagination: {el: '.all-plan-pagination'}
-  });
+  },
+  pagination: {
+    el: ".all-plan-pagination",
+  },
+});
 
-  var hiwSwiper = new Swiper(".all-hiw-science-cards", {
-     // configure Swiper to use modules
-  
-    slidesPerView: "auto",
-    spaceBetween: 20,
-    centerInsufficientSlides: true,
-    breakpoints: {
-      // when window width is >= 768px
-      768: {
-        spaceBetween: 32,
-      },
+// hiwSwiper
+const hiwSwiper = initSwiper(".all-hiw-science-cards", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  centerInsufficientSlides: true,
+  breakpoints: {
+    768: {
+      spaceBetween: 32,
     },
-    pagination: {
-      el: ".hiw-swiper-pagination",
-    },
-  });
+  },
+  pagination: {
+    el: ".hiw-swiper-pagination",
+  },
+});
 
-  // Success carousel
-var successThumbs = new Swiper(".all-success-thumbs", {
-    // configure Swiper to use modules
-   
-    slidesPerView: "auto",
-    spaceBetween: 20,
-    allowSlideNext: false,
-    allowSlidePrev: false,
-    threshold: 12,
-    freeMode: true,
-    watchSlidesProgress: true,
-    centerInsufficientSlides: true,
-  });
-  var successSwiper = new Swiper(".all-success-stories", {
-    // configure Swiper to use modules
- 
-    slidesPerView: 1,
-    spaceBetween: 20,
-    breakpoints: {
-        770: {
-          slidesPerView: "auto",
-            spaceBetween: 60,
-            centerInsufficientSlides: true,
-      },
+// successThumbs
+const successThumbsSwiper = initSwiper(".all-success-thumbs", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  allowSlideNext: false,
+  allowSlidePrev: false,
+  threshold: 12,
+  freeMode: true,
+  watchSlidesProgress: true,
+  centerInsufficientSlides: true,
+});
+
+// successSwiper
+const successSwiper = initSwiper(".all-success-stories", {
+  slidesPerView: 1,
+  spaceBetween: 20,
+  breakpoints: {
+    770: {
+      slidesPerView: "auto",
+      spaceBetween: 60,
+      centerInsufficientSlides: true,
     },
-    thumbs: {
-      swiper: successThumbs,
-    },
-  });
-  
-  var confidentSwiper = new Swiper(".confident-members-swiper", {
-    // configure Swiper to use modules
-    
-    slidesPerView: "auto",
-    spaceBetween: 20,
-    centerInsufficientSlides: true,
-    pagination: {
-      el: ".confident-members-pagination",
-    },
-  });
+  },
+  thumbs: {
+    swiper: successThumbsSwiper,
+  },
+});
+
+// confidentSwiper
+const confidentSwiper = initSwiper(".confident-members-swiper", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+  centerInsufficientSlides: true,
+  pagination: {
+    el: ".confident-members-pagination",
+  },
+});
