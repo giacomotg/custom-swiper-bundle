8c1ba5db063878e1d9b5103e51195c6a523d598a
Giacomo Ghislandi <giacomotg@gmail.com>
2023-10-01 13:53:58 -0300

    Updated custom_swiper_bundle.js in dist directory

--- dist/custom_swiper_bundle.js
+++ dist/custom_swiper_bundle.js
@@ -1,3 +1,6 @@
-import Swiper, { Navigation, Pagination } from 'swiper';
+// import function to register Swiper custom elements
+import { register } from 'swiper/element/bundle';
+// register Swiper custom elements
+register();
 
 import 'swiper-bundle.min.css'
